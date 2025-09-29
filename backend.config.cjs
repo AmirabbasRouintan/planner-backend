ef91aafdef69de93b9ead9b8d549887cdb41f5ff
AmirabbasRouintan <amirabbas.helma8693@gmail.com>
2025-09-30 00:30:19 +0330

    done host back and front

--- backend.config.cjs
+++ backend.config.cjs
@@ -30,7 +30,7 @@ const getBackendUrl = () => {
   }
   
   // Default fallback
-  const defaultUrl = 'http://192.168.100.65:8000';
+  const defaultUrl = 'http://ixiflower32.pythonanywhere.com';
   console.log(`Using default backend URL: ${defaultUrl}`);
   return defaultUrl;
 };
